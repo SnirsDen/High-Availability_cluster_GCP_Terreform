e80f9a46d0ea54810ea65f5cba69e0aef7904f36
SnirsDen <fannaruta@mail.ru>
2025-08-28 09:44:42 +0300

    terraform_monitoring

---
+++ test2/terraform.tfvars
@@ -0,0 +1,1 @@
+project_id = "cryptic-heaven-462819-i6"
---
+++ test2/variables.tf
@@ -0,0 +1,39 @@
+variable "project_id" {
+  description = "cryptic-heaven-462819-i6"
+  type        = string
+}
+
+variable "region" {
+  description = "Основной регион GCP"
+  type        = string
+  default     = "europe-west4"
+}
+
+variable "prod_zones" {
+  description = "Зоны для PROD (europe-west4)"
+  type        = list(string)
+  default = [
+    "europe-west4-a",
+    "europe-west4-b"
+  ]
+}
+
+variable "dev_zones" {
+  description = "Зоны для DEV (europe-west3)"
+  type        = list(string)
+  default = [
+    "europe-west3-a",
+    "europe-west3-b"
+  ]
+}
+variable "ssh_private_key_path" {
+  description = "Path to the private SSH key"
+  type        = string
+  default     = "~/.ssh/id_rsa"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to the public SSH key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
